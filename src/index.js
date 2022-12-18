6ecc1bd841cb1537958a065b51ed73c6e1d89be3
Dima <57713733+Dima1409@users.noreply.github.com>
2022-12-18 23:54:02 +0200

    add react-router-dom, created pages Homepage, Form Login, Form Registration and PhoneBook page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import App from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter basename='goit-react-hw08-phonebook'>
     <Provider store={store}>
        <App />
     </Provider>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import { Title } from "./Home.styled";
+
+const Home = () => {
+    return (
+        <Title>Welcome to Phonebook!</Title>
+    )
+}
+
+export default Home;
